6e06e2c25d819f3adee42dc2a967fc73e0faeee2
Basile Vieu <basile.vieu@zerogamesstudios.com>
2026-07-09 13:41:52 +0200

    Update Target.cs files to 5.8

--- Source/CompendiumEditor.Target.cs
+++ Source/CompendiumEditor.Target.cs
@@ -8,8 +8,8 @@ public class CompendiumEditorTarget : TargetRules
 	public CompendiumEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("Compendium");
 	}
 }
